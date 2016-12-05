f7a24ed899f8a1004d7beedb7ef382cae2be5520
Tom <sedevelopers01+thwd@gmail.com>
2016-12-05 13:24:49 +0100

    restructured TCP code, deleted unnecessary UDP code.

--- server/main.go
+++ server/main.go
@@ -3,23 +3,35 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/satori/go.uuid"
 	"log"
 	"net"
 	"os"
 	"sync"
-
-	"github.com/satori/go.uuid"
 )
 
-var players = make(map[uuid.UUID]string)
-var playerLock = new(sync.RWMutex)
-
 const (
-	UDP_PORT    = `7604`
-	UDP_BUF_CAP = 1024
 	TCP_PORT    = `7606`
 	TCP_BUF_CAP = 1024
 )
+
+var (
+	TcpByteBufferPool = &sync.Pool{
+		New: func() interface{} {
+			return make([]byte, TCP_BUF_CAP, TCP_BUF_CAP)
+		},
+	}
+)
+
+type JSON []byte
+
+func (j JSON) MarshalJSON() ([]byte, error) {
+	return j, nil
+}
+func (j *JSON) UnmarshalJSON(data []byte) error {
+	*j = append((*j)[:0], data...)
+	return nil
+}
 
 func exitOnError(e error) {
 	if e != nil {
@@ -28,75 +40,122 @@ func exitOnError(e error) {
 	}
 }
 
-func listenUDP() {
-	socket, e := net.ResolveUDPAddr(`udp`, ":"+UDP_PORT)
-	exitOnError(e)
+type GeneralResponse struct {
+	Error    *ErrorResponse `json:"error,omitempty"`
+	Response interface{}    `json:"response,omitempty"`
+}
 
-	conn, e := net.ListenUDP(`udp`, socket)
-	exitOnError(e)
+type ErrorResponse struct {
+	Type    string      `json:"type"`
+	Message string      `json:"message"`
+	Payload interface{} `json:"payload"`
+}
 
-	defer conn.Close()
+type TcpRequest struct {
+	Action  string `json:"action"`
+	Payload JSON   `json:"payload"`
+}
 
-	buffer := make([]byte, UDP_BUF_CAP, UDP_BUF_CAP)
+func decodeRequest(conn net.Conn) (string, JSON, *ErrorResponse) {
 
-	for {
-		n, remAddr, e := conn.ReadFromUDP(buffer)
-		if e != nil {
-			log.Println(remAddr, e)
-			continue
+	buffer := TcpByteBufferPool.Get().([]byte)
+	defer TcpByteBufferPool.Put(buffer)
+
+	n, e := conn.Read(buffer)
+	if e != nil {
+		return "", nil, &ErrorResponse{
+			Type:    "network",
+			Message: "failed reading from socket",
+			Payload: nil,
 		}
-		payload := buffer[:n]
-		log.Println("received", string(payload), "from", remAddr)
+	}
+
+	payload := buffer[:n]
+	request := TcpRequest{}
+	if e := json.Unmarshal(payload, &request); e != nil {
+		return "", nil, &ErrorResponse{
+			Type:    "request",
+			Message: "failed decoding JSON",
+			Payload: string(payload),
+		}
+
+	}
+
+	return request.Action, request.Payload, nil
+}
+
+func dispatch(action string, payload JSON) (interface{}, *ErrorResponse) {
+	switch action {
+	case "join":
+		joinReq := JoinRequest{}
+		if e := json.Unmarshal(payload, &joinReq); e != nil {
+			return nil, &ErrorResponse{
+				Type:    "request",
+				Message: "failed decoding JSON join request",
+				Payload: string(payload),
+			}
+		}
+		return HandleJoin(joinReq)
+	}
+	return nil, &ErrorResponse{
+		Type:    "request",
+		Message: "no such action",
+		Payload: action,
 	}
 }
 
-func listenTCP() {
-	l, e := net.Listen(`tcp`, ":"+TCP_PORT)
+func serveTCP() {
+
+	socket, e := net.Listen(`tcp`, ":"+TCP_PORT)
 	if e != nil {
-		fmt.Println("Error listening:", e.Error())
-		os.Exit(1)
+		exitOnError(fmt.Errorf("Error listening on port %s: %s", TCP_PORT, e.Error()))
 	}
-	// Close the listener when the application closes.
-	defer l.Close()
-	fmt.Println("Listening on " + ":" + TCP_PORT)
-	buffer := make([]byte, TCP_BUF_CAP, TCP_BUF_CAP)
+	defer socket.Close()
+	log.Println("Listening on TCP port", TCP_PORT)
+
 	for {
-		conn, e := l.Accept()
+
+		conn, e := socket.Accept()
 		if e != nil {
 			log.Println("Error accepting: ", e)
+			if conn != nil {
+				conn.Close()
+			}
 			continue
 		}
-		n, e := conn.Read(buffer)
-		if e != nil {
-			log.Println("Error reading: ", e)
+
+		action, payload, err := decodeRequest(conn)
+		if err != nil {
+			json.NewEncoder(conn).Encode(GeneralResponse{Error: err})
+			conn.Close()
 			continue
 		}
-		payload := buffer[:n]
 
-		id := uuid.NewV4()
-		player := struct {
-			Name string `json:"name"`
-			Id   string `json:"id"`
-		}{}
-
-		json.Unmarshal(payload, &player)
-
-		log.Println("received", string(payload), "from", conn.RemoteAddr(), "length: ", len(payload), "id: ", id.String())
-
-		playerLock.Lock()
-		players[id] = player.Name
-		playerLock.Unlock()
-
-		player.Id = id.String()
-
-		json.NewEncoder(conn).Encode(player)
+		go func() {
+			res, err := dispatch(action, payload)
+			if err != nil {
+				json.NewEncoder(conn).Encode(GeneralResponse{Error: err})
+			} else {
+				json.NewEncoder(conn).Encode(GeneralResponse{Response: res})
+			}
+			conn.Close()
+		}()
 
 	}
+}
+
+type JoinRequest struct {
+	Name string `json:"name"`
+	Port int    `json:"port"` // for UDP
+}
+type JoinResponse struct {
+	Id string `json:"id"`
+}
+
+func HandleJoin(req JoinRequest) (JoinResponse, *ErrorResponse) {
+	return JoinResponse{Id: uuid.NewV4().String()}, nil
 }
 
 func main() {
-	go listenTCP()
-	go listenUDP()
-	select {}
-
+	serveTCP()
 }

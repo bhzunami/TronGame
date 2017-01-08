fb5ee60e3a3150044804601c461111b3c08c0ea9
Nicolas Mauchle <nicolas@nicolasmauchle.ch>
2017-01-08 15:38:24 +0100

    Add Trace

---
+++ ether-tron/src/main/java/ch/fhnw/model/Point.java
@@ -0,0 +1,29 @@
+package ch.fhnw.model;
+
+import ch.fhnw.ether.scene.mesh.IMesh;
+import ch.fhnw.ether.scene.mesh.MeshUtilities;
+import ch.fhnw.util.math.Mat4;
+import ch.fhnw.util.math.Vec3;
+
+public class Point {
+    
+    private IMesh mesh;
+    private Vec3 position;
+    
+    public Point() {
+        this.position = new Vec3(0f, 0f, -20f);
+        this.mesh = MeshUtilities.createCube();
+        this.mesh.setPosition(this.position);
+    }
+    
+    
+    public void setPosition(Vec3 position) {
+        this.position = position;
+        this.mesh.setPosition(this.position);
+    }
+    
+    public IMesh getMesh() {
+        return this.mesh;
+    }
+
+}
